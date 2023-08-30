5222d0d46edae2293043682e54cc3ee3302e9dad
Chaibouu <chaibouabdoulwahab@gmail.com>
2023-08-30 02:54:18 +0100

    add blague

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 let conteneur = document.querySelector('#conteneur');
+let auteur = document.querySelector('.auteur');
 let btn = document.querySelector('#btn');
 let body = document.body;
 let Index = 0;
@@ -6,11 +7,13 @@ let Index = 0;
 btn.addEventListener('click', () => {
   if (Index < tableau.length) {
     conteneur.textContent = tableau[Index].contenue;
+    auteur.textContent = "⏤" + tableau[Index].auteur;
     Index++;
   } 
   else {
     Index = 0;
     conteneur.textContent = tableau[Index].contenue;
+    auteur.textContent = "⏤" + tableau[Index].auteur;
     Index++;
   }
 });
